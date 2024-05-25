cb364ba69119d108092959774453b87165effe7a
Michele Bombardi <michele.bombardi@mounthorse.it>
2024-05-25 16:29:49 +0200

    init

---
+++ Willness.Turnstile.Server/reload.sh
@@ -0,0 +1,18 @@
+﻿#!/bin/sh
+
+echo "Stopping service..."
+systemctl stop willness.turnstile.service
+
+echo "Enabling service..."
+systemctl enable willness.turnstile.service
+
+echo "Starting service..."
+systemctl start willness.turnstile.service
+
+echo "Checking nginx synthax..."
+nginx -t
+
+echo "Reloading nginx..."
+nginx -s reload
+
+echo "Done."
